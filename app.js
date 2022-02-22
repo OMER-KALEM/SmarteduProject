bf66d28de45bb14974a41c408054e38da5d56d04
Ömer KALEM <omer.kalem@ogr.sakarya.edu.tr>
2022-02-22 11:15:41 +0300

    Routing, course post

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
+//3rd party
 const express = require('express');
+const mongoose = require('mongoose');
+//nodejs core
+//kendi olusturdugtumuz dosyalar
 const pageRoute = require('./routes/pageRoute');
-const pageController = require('./controllers/pageController');
+const courseRoute = require('./routes/courseRoute');
 
 const app = express();
+
+//DB Connect
+mongoose
+  .connect('mongodb://localhost/smartedu-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('Db connected succesfully');
+  });
 
 //Template Engine
 app.set('view engine', 'ejs');
 
 //Middlewares
 app.use(express.static('public'));
-
-const port = 3000;
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 //Route
 app.use('/', pageRoute);
+app.use('/courses', courseRoute);
 
+const port = 3000;
 app.listen(port, () => {
   console.log(`App stated on port ${port}`);
 });
